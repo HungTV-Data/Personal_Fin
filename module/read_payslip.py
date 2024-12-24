323d8c80e2f7b39d3fe73fd3fbf0d56ef4ee8aed
JohnTran <yaibatran1999@gmail.com>
2024-12-24 17:54:48 +0700

    sync file

--- module/read_payslip.py
+++ module/read_payslip.py
@@ -31,7 +31,7 @@ class pdfReader():
                         tables.append(pd.DataFrame(table))
         return tables
 
-    def clean_headers(headers):
+    def clean_headers(self, headers):
         """Clean column headers by removing Vietnamese words stand before slash and replacing spaces with underscores."""
         cleaned = []
         for header in headers:
@@ -66,31 +66,60 @@ class pdfReader():
 
 
 class payslipReader(pdfReader):
-    def __init__(self, pdf_path, path_column_schema="VNG_payslip.json"):
+    def __init__(self, pdf_path, path_column_schema=r"D:\\Hungtv7\\Personal_Fin\\config\\VNG_payslip.json"):
         super().__init__(pdf_path, path_column_schema)
 
+    def special_case_for_header(header):
+        if header == 'Công ty':
+            return "Company"
+        # elif header == "Tổng số ngày nghỉ hưởng lương BHXH (tỷ lệ 75%)":
+        #     return "Leave days subject to 75% Social Insurance"
+        # elif header == "Tổng số ngày nghỉ hưởng lương BHXH (tỷ lệ 100%)":
+        #     return "Leave days subject to 100% Social Insurance"
+        elif header == "Lương doanh số Commission":
+            return "Comission"
+        else:
+            return header
+    
+    def clean_headers(self, headers):
+        """Clean column headers by removing Vietnamese words stand before slash and replacing spaces with underscores."""
+        cleaned = []
+        for header in headers:
+            # Remove Vietnamese words (containing diacritics) and keep only English words
+            
+            for char in ("/", "\n"):
+                try:
+                    print(header.split(f"{char}")[1])
+                    english_only = payslipReader.special_case_for_header(header.split(f"{char}")[1])
+                except IndexError as e:
+                    continue
+                    
+            # print(english_only.strip().replace(' ', '_'))
+            cleaned.append(english_only.strip().replace(' ', '_'))  # Replace spaces with underscores
+        return cleaned
+        
+    def extract_tables(self):
+        """Extract tables from the PDF file."""
+        with pdfplumber.open(self._pdfReader__pdf_path) as pdf:
+            tables = []
+            for page in pdf.pages:
+                page_tables = page.extract_tables()
+                for table in page_tables:
+                    if table:
+                        df = pd.DataFrame(table[1:], columns=table[0]).transpose()
+                        df.reset_index(inplace=True)
+                        df.columns = self.clean_headers(df.iloc[0])  # Clean headers
+                        df = df.drop(df.index[0])  # Drop the row used for column names
+                        tables.append(df)
+        return tables
 
-# def special_case_for_header(header):
-#     if header == 'Công ty':
-#         return "Company"
-#     # elif header == "Tổng số ngày nghỉ hưởng lương BHXH (tỷ lệ 75%)":
-#     #     return "Leave days subject to 75% Social Insurance"
-#     # elif header == "Tổng số ngày nghỉ hưởng lương BHXH (tỷ lệ 100%)":
-#     #     return "Leave days subject to 100% Social Insurance"
-#     elif header == "Lương doanh số Commission":
-#         return "Comission"
-#     else:
-#         return header
 
+vng_payslip_reader = payslipReader(r"D:\Hungtv7\Personal_Fin\VNG_payslip\payslip_VG-15316_2021_04.pdf")
 
-# Path to the uploaded PDF file
-pdf_path = r"D:\Hungtv7\Personal_Fin\VNG_payslip\payslip_VG-15316_2021_04.pdf"
-
-# Extract tables from the PDF file
-tables = extract_cleaned_tables_with_index(pdf_path)
+transposed_tables = vng_payslip_reader.extract_tables()
 
 # Display the first few rows of each extracted table for illustration
-for i, table in enumerate(tables):
+for i, table in enumerate(transposed_tables):
     if table.shape[0] < 3:
         table.drop([1])   
     elif table.shape[0] >= 3:
